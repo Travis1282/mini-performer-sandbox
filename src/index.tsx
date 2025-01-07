527d3da48ffdc9863aaf694636862004bfdc1179
Kelly Milligan <kelly.milligan@gmail.com>
2025-01-07 10:10:19 -0600

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import { Hono } from "hono";
-// import { renderer } from "./renderer";
 import { renderToString } from "react-dom/server";
 
 const app = new Hono();
-
-// app.use(renderer);
 
 app.get("*", (c) => {
   return c.html(
