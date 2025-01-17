f5409061c6fbbfbbe13ce81e43adbd87aa7616ca
Kelly Milligan <kelly.milligan@gmail.com>
2025-01-17 16:00:49 -0600

    ignore sort imports for tests, but sort the rest

--- README.md
+++ README.md
@@ -9,8 +9,9 @@ npm run deploy
 
 ## TODO
 
-- [ ] add eslint
-- [ ] add prettier
 - [ ] husky and lint-staged
 - [ ] add growthbook
 - [ ] add GTM, other root script tags
+- [ ] add env vars, figure out deployments and env vars
+- [ ] add vitest
+- [ ] testing, linting, type checking on prs
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 
 import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import perfectionist from 'eslint-plugin-perfectionist'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   {
@@ -18,12 +19,32 @@ export default tseslint.config(
   {
     ignores: ['dist/', 'node_modules/', 'src/services/maverick/generated/'],
   },
+  {
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      'perfectionist/sort-imports': 'error',
+      'perfectionist/sort-interfaces': 'error',
+      'perfectionist/sort-jsx-props': 'error',
+      'perfectionist/sort-union-types': 'error',
+      'perfectionist/sort-named-imports': 'error',
+    },
+  },
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   {
     rules: {
       'no-useless-catch': 'off',
+      curly: 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
+  {
+    files: ['**/*.test.ts', '**/*.test.tsx'],
+    rules: {
+      'perfectionist/sort-imports': 'off',
     },
   }
 )
