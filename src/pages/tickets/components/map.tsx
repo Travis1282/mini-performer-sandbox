034b8104250ccfbf17b66ce417d242b6f8af84b8
Kelly Milligan <kelly.milligan@gmail.com>
2025-01-17 16:07:17 -0600

    remove css that was interfering with panzoom

--- src/pages/tickets/components/map.tsx
+++ src/pages/tickets/components/map.tsx
@@ -1,4 +1,4 @@
-import type { PanZoom } from 'panzoom';
+import type { PanZoom } from 'panzoom'
 
 import panzoom from 'panzoom'
 import { useEffect, useRef, useState } from 'react'
@@ -34,17 +34,12 @@ export const Map = ({ mapSrc }: MapProps) => {
     const panZoom = panzoom(svgGroup as SVGElement, {
       bounds: true,
       boundsPadding: 0.1,
-      maxZoom: 4,
-      minZoom: 0.5,
     })
     panZoomRef.current = panZoom
   }, [mapLoaded])
 
   return (
-    <div
-      className="flex h-[17rem] w-full origin-center transform-gpu touch-none items-center justify-center bg-slate-200 p-1"
-      ref={mapRef}
-    >
+    <div className="flex h-[17rem] w-full bg-slate-200 p-1" ref={mapRef}>
       <SVG
         className="h-full w-full"
         onLoad={() => {
