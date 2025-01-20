a41a624aeae719c6f9929d13dea53c7792991b98
Kelly Milligan <kelly.milligan@gmail.com>
2025-01-20 13:47:58 -0600

    strip it back down

--- src/pages/tickets/tickets.loader.ts
+++ src/pages/tickets/tickets.loader.ts
@@ -1,15 +1,23 @@
-import type { LoaderFunctionArgs } from 'react-router'
-import { getEventMetadata } from '@/services/maverick/getEventMetadata'
+// import type { LoaderFunctionArgs } from 'react-router'
+// import { getEventMetadata } from '@/services/maverick/getEventMetadata'
 
-export function loader({ request, params }: LoaderFunctionArgs) {
-  return getEventMetadata({
-    init: {
-      signal: request.signal,
-    },
-    params: {
-      path: {
-        'event-id': Number(params.eventId),
-      },
+// export function loader({ request, params }: LoaderFunctionArgs) {
+//   return getEventMetadata({
+//     init: {
+//       signal: request.signal,
+//     },
+//     params: {
+//       path: {
+//         'event-id': Number(params.eventId),
+//       },
+//     },
+//   })
+// }
+export function loader() {
+  return Promise.resolve({
+    data: {
+      id: 1,
+      name: 'Test',
     },
   })
 }
