f234e2273f83116bd688182206961dfb2ffa852e
Kelly Milligan <kelly.milligan@gmail.com>
2025-01-30 15:18:28 -0600

    get back to working state

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ const app = new Hono()
 
 app.use(logger())
 
-app.all('/rest/*', basicProxy(import.meta.env.VITE_MAVERICK_URL))
+app.get('/rest/*', basicProxy(import.meta.env.VITE_MAVERICK_URL))
 
 app.get('*', async (c) => {
   const { ip, loc, latitude, longitude } = getIpAndLoc(c.req.raw)
