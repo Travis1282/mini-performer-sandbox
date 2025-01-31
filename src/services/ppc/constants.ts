f99107ef173c0367255cfd1a4b319e98f304a2c6
Kelly Milligan <kelly.milligan@gmail.com>
2025-01-30 19:46:22 -0600

    change prod env to app env preview to send CF headers

--- src/services/ppc/constants.ts
+++ src/services/ppc/constants.ts
@@ -1,3 +1,5 @@
+import { isProd } from '../config'
+
 export const PARAMS_TO_CHECK = [
   'utm_source',
   'utm_medium',
@@ -19,7 +21,6 @@ export const SESSION_COOKIE = 'gt_sid'
 export const PROFILE_COOKIE = 'gt_pid'
 export const PREVIOUS_PARAMS_COOKIE = 'gt_up'
 export const COOKIE_EXPIRY_DAYS = 90
-export const IS_PROD = import.meta.env.VITE_APP_ENV === 'production'
 export const COOKIE_DOMAIN =
   import.meta.env.VITE_COOKIE_DOMAIN ?? '.gotickets.com'
-export const COOKIE_SAME_SITE = IS_PROD ? 'lax' : 'none'
+export const COOKIE_SAME_SITE = isProd ? 'lax' : 'none'
